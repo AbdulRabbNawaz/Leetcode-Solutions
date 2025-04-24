b753995bd695f3143da8a11a63e73e70f960dbe4
Abdul Rabb Nawaz <Abdulrabb41@gmail.com>
2025-04-24 09:47:18 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (83.43%) - LeetSync

--- Arrays/73-set-matrix-zeroes/set-matrix-zeroes.java
+++ Arrays/73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,58 +1,54 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
+
         int n = matrix.length;
         int m = matrix[0].length;
 
-        boolean firstRowZero = false;
-        boolean firstColZero = false;
+        boolean firstZeroRow = false;
+        boolean firstZeroCol = false;
 
-        // Step 1: Check if first row has a zero
-        for (int j = 0; j < m; j++) {
-            if (matrix[0][j] == 0) {
-                firstRowZero = true;
+        for(int i = 0; i < m; i++){
+            if(matrix[0][i] == 0){
+                firstZeroRow = true;
                 break;
             }
         }
 
-        // Step 1: Check if first column has a zero
-        for (int i = 0; i < n; i++) {
-            if (matrix[i][0] == 0) {
-                firstColZero = true;
+        for(int j = 0; j < n; j++){
+            if(matrix[j][0] == 0){
+                firstZeroCol = true;
                 break;
             }
         }
 
-        // Step 2: Use first row and column as flags
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < m; j++) {
-                if (matrix[i][j] == 0) {
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j < m; j++){
+                if(matrix[i][j] == 0){
                     matrix[i][0] = 0;
                     matrix[0][j] = 0;
                 }
             }
         }
 
-        // Step 3: Set matrix[i][j] to 0 if flagged
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < m; j++) {
-                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j < m; j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
                     matrix[i][j] = 0;
                 }
             }
         }
 
-        // Step 4: Set first row to 0 if needed
-        if (firstRowZero) {
-            for (int j = 0; j < m; j++) {
+        if(firstZeroRow){
+            for(int j = 0; j < m; j++){
                 matrix[0][j] = 0;
             }
         }
 
-        // Step 4: Set first column to 0 if needed
-        if (firstColZero) {
-            for (int i = 0; i < n; i++) {
-                matrix[i][0] = 0;
+        if(firstZeroCol){
+            for(int i = 0; i < n; i++){
+            matrix[i][0] = 0;
             }
         }
+        
     }
 }
